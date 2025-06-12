42bd38259f70b04c27376d9330a816b7bb2426f8
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-06-13 00:24:46 +0600

    header section ready to use in multiple pages

---
+++ app/views/Layouts/header.php
@@ -0,0 +1,24 @@
+<?php
+echo <<<HTML
+<header class="header">
+  <nav class="nav-bar">
+    <div>
+      <img src="" alt="" />
+      <p class="title1">RevoTV <small class="title2">+</small></p>
+    </div>
+    <div>
+      <input type="search" placeholder="search RevoTV" name="" id="" />
+      <button class="btn-nav" onclick="return adv_search()">ASearch</button>
+      <button class="btn-nav">Actor Profile</button>
+      <button class="btn-nav"><a class="" href="">Watchlist</a></button>
+      <button class="btn-nav"><a href="">Signin</a></button>
+    </div>
+  </nav>
+
+  <section id="adv-search"></section>
+  <section id="adv-search-results" class="container">
+    <div id="adv-search-result-container"></div>
+  </section>
+</header>
+HTML;
+?>
