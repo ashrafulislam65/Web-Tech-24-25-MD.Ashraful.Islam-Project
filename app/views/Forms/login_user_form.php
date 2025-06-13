4eef2c69b4fb0a4a811253a97996b6ccf1199f53
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-06-13 07:28:28 +0600

    header and footer now finally dynamic

--- app/views/Forms/login_user_form.php
+++ app/views/Forms/login_user_form.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../index.css" />
     
 </head>
 <body>
     <!--Header  -->
+    <?php include '../Layouts/header.php';?>
      hi i am login page
+     <?php include '../Layouts/footer.php';?>
      
     
 </body>
